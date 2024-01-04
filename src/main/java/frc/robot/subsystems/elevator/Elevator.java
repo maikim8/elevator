2cf04d9b209fe0a2fdc4b67e078a0f958c4984e7
clairethebear1 <153558931+clairethebear1@users.noreply.github.com>
2024-01-03 19:40:47 -0500

    Update Elevator.java

--- src/main/java/frc/robot/subsystems/elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/elevator/Elevator.java
@@ -21,17 +21,11 @@ import static frc.robot.Constants.Elevator.ElevatorPhysicalConstants.ELEVATOR_PI
 public class Elevator extends SubsystemBase {
     private final ElevatorIOInputsAutoLogged inputs = new ElevatorIOInputsAutoLogged();
 
-    public enum State {
-        MANUAL,
-        PID
-    }
-
     private LoggedDashboardNumber p = new LoggedDashboardNumber("Elevator/P", ELEVATOR_PID[0]);
     private LoggedDashboardNumber i = new LoggedDashboardNumber("Elevator/I", ELEVATOR_PID[1]);
     private LoggedDashboardNumber d = new LoggedDashboardNumber("Elevator/D", ELEVATOR_PID[2]);
     private LoggedDashboardNumber ff = new LoggedDashboardNumber("Elevator/FF", ELEVATOR_PID[3]);
 
-    private State state = State.MANUAL;
     private double setpoint = 0;
 
     private final ElevatorIO io;
@@ -55,7 +49,7 @@ public class Elevator extends SubsystemBase {
 
         ElevatorMechanism.setLength(io.getDistance());
 
-        // Update PID constants
+        // Update the PID constants if they have changed
         if (p.get() != io.getP()) 
             io.setP(p.get());
         
@@ -73,7 +67,7 @@ public class Elevator extends SubsystemBase {
     }
 
     public void setVoltage(double motorVolts) {
-        // limit the elevator if past limit
+        // limit the elevator if its past its limit
         if (io.getDistance() > io.ELEVATOR_MAX_HEIGHT && motorVolts > 0) {
             motorVolts = 0;
         } else if (io.getDistance() < io.ELEVATOR_MIN_HEIGHT && motorVolts < 0) {
@@ -119,6 +113,9 @@ public class Elevator extends SubsystemBase {
             this::atSetpoint, 
             this
         );
+    }
+    
+}
     }
     
 }
